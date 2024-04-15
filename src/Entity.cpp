e9c7d305b8f4998e6bef4e5efd274b050dc65b51
ceogawa <ceogawa@calpoly.edu>
2024-04-15 13:28:28 -0700

    it compiles now... yay

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -21,9 +21,6 @@ void Entity::initEntity(std::vector<std::shared_ptr<Shape>> ref){
     }
 }
 
-void Entity::setObjs(std::vector<std::shared_ptr<Shape>> ref) {   
-}
-
 void Entity::setMaterials(int i, float r1, float g1, float b1, float r2, float g2, float b2, 
     float r3, float g3, float b3, float s) {
         material[i].matAmb.r = r1;
